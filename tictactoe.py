0e7964d04d9f51bb704692ddee62f33e668e724b
chantuot <chanrith.tuot@gmail.com>
2020-10-16 20:23:48 -0700

    init and layout based on input

---
+++ tictactoe.py
@@ -0,0 +1,11 @@
+input = 'O_OXXO_XX'
+container = []
+
+for char in input:
+    container.append(char)
+
+print('---------')
+print('|', container[0], container[1], container[2], '|')
+print('|', container[3], container[4], container[5], '|')
+print('|', container[6], container[7], container[8], '|')
+print('---------')

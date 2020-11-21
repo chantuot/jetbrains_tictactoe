96cbd737a19ebbf4fc42bd79c6b942c0e44fb167
chantuot <chanrith.tuot@gmail.com>
2020-11-21 12:28:11 -0800

    changed sketch

--- sketchbook_ttt.py
+++ sketchbook_ttt.py
@@ -1,13 +1,4 @@
 
-def get_coords():
-    print("Enter coords: ")
-    global coords_x, coords_y
-    coords_x, coords_y = input().split(" ")
-    print(coords_x, coords_y)
-    coords_x = int(coords_x)
-    coords_y = int(coords_y)
-    print('coords x:', type(coords_x), 'coords y:', type(coords_y))
-
 # check cells to see if they are empty or not and replace with X or O if empty
 def check_cell():
     while True:
@@ -15,11 +6,14 @@ def check_cell():
             print("Enter coords: ")
             global coords_x, coords_y
             coords_x, coords_y = input().split(" ")
-            print(coords_x, coords_y)
+            # print(coords_x, coords_y)
             coords_x = int(coords_x)
             coords_y = int(coords_y)
-            print('coords x:', type(coords_x), 'coords y:', type(coords_y))
-            if coords_x == 1:
+            if coords_x < 1 or coords_x > 3 or coords_y < 1 or coords_y >3:
+                print("Coordinates should be from 1 to 3!")
+                check_cell()
+            # print('coords x:', type(coords_x), 'coords y:', type(coords_y))
+            elif coords_x == 1:
                 if coords_y == 1:
                     print(og_board_state_parser[6])
                     if_empty_replace(6)
@@ -49,13 +43,13 @@ def check_cell():
                 elif coords_y == 3:
                     print(og_board_state_parser[2])
                     if_empty_replace(2)
-            elif coords_x < 1 or coords_x > 3:
-                print("Coordinates should be from 1 to 3!")
             else:
-                print("Problem")
+                print("Problem, try again.")
+                check_cell()
             break
         except ValueError:
             print("You should enter numbers")
+            check_cell()
             break
 
 
@@ -65,6 +59,7 @@ def coordinate_filters(coords_x, coords_y): # change function to convert to int 
     elif coords_x != ('1' or '2' or '3'):
         print('You should enter numbers!')
 
+# prints game board status onto console
 def print_board_state():
     print('---------')
     print('|', og_board_state_parser[0], og_board_state_parser[1], og_board_state_parser[2], '|')
@@ -72,6 +67,7 @@ def print_board_state():
     print('|', og_board_state_parser[6], og_board_state_parser[7], og_board_state_parser[8], '|')
     print('---------')
 
+# determins if cell on gameboard is empty and replace with X respectively
 def if_empty_replace(n):
     global og_board_state_parser
     if og_board_state_parser[n] == ('_' or ' '):
@@ -92,21 +88,9 @@ def get_board_state(field_input):
         og_board_state_parser.append(char)
 
 
-    # #Rows, columns, and diagnonals outcomes as a list with each element as a three letter string i.e. 'XXX' or 'XOO'
-    # outcomes = [
-    #     og_board_state[0:3],     #row 1
-    #     og_board_state[3:6],     #row 2
-    #     og_board_state[6:9],     #row 3
-    #     og_board_state[0::3],    #column 1
-    #     og_board_state[1::3],    #column 2
-    #     og_board_state[2::3],    #column 3
-    #     og_board_state[::4],     #diagonal 1
-    #     og_board_state[2:7:2]    #diagonal 2
-    # ]
-
-
 og_board_state_parser = []
-
+get_board_state("         ")
+print_board_state()
 board_input = input("Enter cells:")
 get_board_state(board_input)
 print_board_state()
@@ -127,3 +111,5 @@ print_board_state()
     #     print("Need more information to fill a 3 X 3 board. Try again.")
     # else:
     #     return og_board_state_parser
+
+#do board states next
